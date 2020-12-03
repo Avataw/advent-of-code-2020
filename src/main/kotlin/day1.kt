54b2f54f4186f9f63a6cb17531e2f454069dfb22
Avataw <avataw.dev@gmail.com>
2020-12-03 21:03:45 +0100

    Initial Commit

---
+++ src/main/kotlin/day1.kt
@@ -0,0 +1,7 @@
+class day1 {
+
+    fun test(): Boolean {
+        return true
+    }
+
+}
---
+++ src/test/kotlin/day1Test.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class day1Test {
+
+    @Test
+    fun shouldBeTrue() {
+        val cut = day1()
+        assertTrue(cut.test())
+    }
+}
