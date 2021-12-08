b94714ac16c3664467ac170761ffae4ef6dbb761
Andre Wruszczak <avataw.dev@gmail.com>
2021-12-09 00:51:05 +0100

    Add day 3

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,61 @@
+package day3
+
+data class Toboggan(val xSpeed: Int, val ySpeed: Int) {
+    var x: Int = 0
+    var y: Int = 0
+
+    fun move() {
+        x += xSpeed
+        y += ySpeed
+    }
+}
+
+class Geology(input: List<String>) {
+    private var map: List<List<String>>
+
+    init {
+        map = input.map { it.split("").filter { s -> s.isNotEmpty() } }
+    }
+
+    private fun makeBigger() {
+        map = map.map {
+            listOf(it, it).flatten()
+        }
+    }
+
+    fun countTreesHit(toboggan: Toboggan): Int {
+        var treesHit = 0;
+
+        while (toboggan.y < map.size) {
+            if (map[toboggan.y][toboggan.x] == "#") {
+                treesHit++
+            }
+            if (map[0].size <= toboggan.x + toboggan.xSpeed) {
+                makeBigger()
+            }
+            toboggan.move()
+        }
+        return treesHit
+    }
+}
+
+fun solveA(input: List<String>): Int {
+    val toboggan = Toboggan(3, 1)
+
+    val geology = Geology(input)
+    return geology.countTreesHit(toboggan);
+}
+
+fun solveB(input: List<String>): Long {
+    val toboggans = listOf(
+        Toboggan(1,1),
+        Toboggan(3,1),
+        Toboggan(5,1),
+        Toboggan(7,1),
+        Toboggan(1,2)
+    )
+
+    val geology = Geology(input);
+
+    return toboggans.map { geology.countTreesHit(it).toLong() }.reduce { prod, element -> prod * element }
+}
---
+++ src/test/kotlin/day3/TestDay3.kt
@@ -0,0 +1,65 @@
+import day3.solveA
+import day3.solveB
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TestDay3 {
+
+    @Test
+    fun `should work for the given test input`() {
+        val input = listOf(
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#"
+        )
+
+        val solution = solveA(input)
+        assertEquals(7, solution)
+    }
+
+    @Test
+    fun `should work for the actual input`() {
+        val input = readFileAsLinesUsingUseLines(3)
+
+        val solution = solveA(input)
+        assertEquals(198, solution)
+    }
+
+    @Test
+    fun `should work with b for the given test input`() {
+        val input = listOf(
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#"
+        )
+
+        val solution = solveB(input)
+        assertEquals(336, solution)
+    }
+
+    @Test
+    fun `should work with b for the actual input`() {
+        val input = readFileAsLinesUsingUseLines(3)
+
+        val solution = solveB(input)
+        assertEquals(5140884672, solution)
+    }
+}
+
+
